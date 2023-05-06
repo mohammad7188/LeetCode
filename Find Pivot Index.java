efe91181736f34dbbb066d1f7761ec245365fc7a
Malavya Raval <118731394+MalavyaRaval@users.noreply.github.com>
2023-05-06 12:30:34 -0700

    Create Find Pivot Index.java

---
+++ Find Pivot Index.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int sum = 0;
+        int left = 0;
+        for(int a :nums) sum += a;
+        for(int i = 0;i<nums.length;i++){
+            if(left == sum-left -nums[i]) return i;
+            left = left+nums[i];
+        }
+        return -1;
+    }
+}
